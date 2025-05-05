d96cbe19dba75d98d86e5b5ee6a065d06296c042
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2025-05-05 23:31:48 +0300

    added test to check if weather api cache ttl is configured

--- laravel-react/tests/Unit/Services/WeatherTest.php
+++ laravel-react/tests/Unit/Services/WeatherTest.php
@@ -9,6 +9,10 @@ test('Weather api key is configured', function () {
     expect(config()->get('services.weather.api_key'))->not()->toBeNull();
 });
 
+test('Weather api cache ttl configured', function () {
+    expect(config()->get('services.weather.cache_ttl'))->not()->toBeNull();
+});
+
 test('Weather service throws exception when no api key provided', function () {
     config()->set('services.weather.api_key', null);
     $weatherService = resolve(WeatherService::class);
