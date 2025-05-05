0f2c66e4e94a5abf210d96b5f68b69968b34a1cf
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2025-05-05 23:59:20 +0300

    added more weather feature tests

--- laravel-react/tests/Feature/WeatherTest.php
+++ laravel-react/tests/Feature/WeatherTest.php
@@ -1,31 +1,53 @@
 <?php
 
 
+use App\Services\WeatherService;
+use Illuminate\Support\Facades\Cache;
+
 test('Weather page is reachable', function () {
     $this->get('/')
         ->assertStatus(200);
 });
 
-test('Weather json route requires city query', function () {
-    $this->get('/weather')
-        ->assertStatus(400)
-        ->assertExactJson(['error' => 'Invalid city provided']);
+describe('Weather json route', function () {
+    test('Requires city query', function () {
+        $this->get('/weather')
+            ->assertStatus(400)
+            ->assertExactJson(['error' => 'Invalid city provided']);
+    });
+
+    test('Returns proper data', function () {
+        $this->get('/weather?city=London')
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                'data' => [
+                    'city',
+                    'country',
+                    'temperature',
+                    'condition',
+                    'humidity',
+                    'windSpeed',
+                    'lastUpdated',
+                ]
+            ]);
+    });
+
+    test('Stores data in cache', function () {
+        Cache::flush();
+
+        $cacheKey = WeatherService::CACHE_KEY . 'London';
+
+        expect(Cache::has($cacheKey))->toBeFalse();
+
+        $this->get('/weather?city=London')
+            ->assertStatus(200);
+
+        expect(Cache::has($cacheKey))->toBeTrue();
+
+        Cache::forget($cacheKey);
+    });
 });
 
-test('Weather json route return weather', function () {
-    $this->get('/weather?city=London')
-        ->assertStatus(200)
-        ->assertJsonStructure([
-            'data' => [
-                'city',
-                'country',
-                'temperature',
-                'condition',
-                'humidity',
-                'windSpeed',
-                'lastUpdated',
-            ]
-        ]);
-});
+
 
 
